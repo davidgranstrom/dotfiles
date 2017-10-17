dee9b0fce14baacab53d75f9dba80c6dd69b9afa
David Granstrom <david@davidgranstrom.com>
2017-10-17 13:55:37 +0200

    Update aliases

--- aliases.sh
+++ aliases.sh
@@ -1,10 +1,10 @@
 alias ..='cd ..'
 
-# ls
-alias ls='ls -G'      # enable color output
-alias l='ls -1F'      # one column, show type
-alias la='ls -laFh'   # long list, show almost all, show type, human readable
-alias lt='ls -laFht'   # long list,sorted by date,show type,human readable
+alias ls='ls -G'                              # enable color output
+alias l='ls -1F'                              # one column, show type
+alias la='ls -laFh'                           # long list, show almost all, show type, human readable
+alias lt='ls -laFht'                          # long list, sorted by date, show type, human readable
+alias li='ls | fzf --preview="head -n 20 {}"' # list and preview
 
 alias rm='rm -i'
 alias cp='cp -i'
