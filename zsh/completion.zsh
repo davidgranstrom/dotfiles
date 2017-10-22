e1b1a26af1a28a3a203bf7e638eab17661a48b0f
David Granström <david@davidgranstrom.com>
2017-10-22 15:44:14 +0200

    Speed up startup time by using cached completion source

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -1,23 +1,39 @@
-# Use modern completion system
+# On slow systems, checking the cached .zcompdump file to see if it must be
+# regenerated adds a noticable delay to zsh startup.  This little hack restricts
+# it to once a day.  It should be pasted into your own completion file.
+#
+# The globbing is a little complicated here:
+# - '#q' is an explicit glob qualifier that makes globbing work within zsh's [[ ]] construct.
+# - 'N' makes the glob pattern evaluate to nothing when it doesn't match (rather than throw a globbing error)
+# - '.' matches "regular files"
+# - 'mh+24' matches files (or directories or whatever) that are older than 24 hours.
+
 autoload -Uz compinit
-compinit
+
+re_initialize=0
+zcompdump=$HOME/.zcompdump
+
+for match in "$zcompdump"*(.Nmh+24); do
+  re_initialize=1
+  break
+done
+
+if [[ "$re_initialize" -eq "1" ]]; then
+  compinit
+  # update the timestamp on compdump file
+  compdump
+else
+  # omit the check for new functions since we updated today
+  compinit -C
+fi
 
 # Do menu-driven completion.
 zstyle ':completion:*' menu select
 
 # Color completion for some things.
 # http://linuxshellaccount.blogspot.com/2008/12/color-completion-using-zsh-modules-on.html
-zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+# zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
 
-source ~/.local/bin/aws_zsh_completer.sh
+# source ~/.local/bin/aws_zsh_completer.sh
 # tmuxp completion
-eval "$(_TMUXP_COMPLETE=source tmuxp)"
-
-# formatting and messages
-# http://www.masterzen.fr/2009/04/19/in-love-with-zsh-part-one/
-# zstyle ':completion:*' verbose yes
-# zstyle ':completion:*:descriptions' format "$fg[yellow]%B--- %d%b"
-# zstyle ':completion:*:messages' format '%d'
-# zstyle ':completion:*:warnings' format "$fg[red]No matches for:$reset_color %d"
-# zstyle ':completion:*:corrections' format '%B%d (errors: %e)%b'
-# zstyle ':completion:*' group-name ''
+# eval "$(_TMUXP_COMPLETE=source tmuxp)"
